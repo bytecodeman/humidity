b90ad62ee3ab942daf9b95eed9b24720f95d66e2
Tony Silvestri <tonysilvestri@bytecodeman.com>
2022-08-09 12:51:45 -0400

    Fixed Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,21 @@
 function Footer() {
   return (
-    <section id="footer" className="flex justify-center">
+    <section id="footer" className="flex justify-center mb-24">
       <div>
         <h3 className="font-bold mb-2">Equations Used:</h3>
-        <p className="font-mono mb-2">
-          RH=100*(EXP((17.625*TD)/(243.04+TD))/EXP((17.625*T)/(243.04+T)))
-          <br />
-          TD=243.04*(LN(RH/100)+((17.625*T)/(243.04+T)))/(17.625-LN(RH/100)-((17.625*T)/(243.04+T)))
-          <br />
-          T=243.04*(((17.625*TD)/(243.04+TD))-LN(RH/100))/(17.625+LN(RH/100)-((17.625*TD)/(243.04+TD
-        </p>
+        <div className="font-mono mb-2">
+          <p className="mb-2">
+            RH = 100*(EXP((17.625*TD)/(243.04+TD))/EXP((17.625*T)/(243.04+T)))
+          </p>
+          <p className="mb-2">
+            TD =
+            243.04*(LN(RH/100)+((17.625*T)/(243.04+T)))/(17.625-LN(RH/100)-((17.625*T)/(243.04+T)))
+          </p>
+          <p className="mb-5">
+            T =
+            243.04*(((17.625*TD)/(243.04+TD))-LN(RH/100))/(17.625+LN(RH/100)-((17.625*TD)/(243.04+TD)))
+          </p>
+        </div>
         <ul className="list-disc list-inside">
           <li>
             T and TD inputs/outputs to the equations are in <b>Celsius</b>
