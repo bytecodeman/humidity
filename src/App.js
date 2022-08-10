07e060ce85f935df7eb5a6c1893b55ee27620224
Tony Silvestri <tonysilvestri@bytecodeman.com>
2022-08-10 06:24:51 -0400

    Removed unnecessary App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import JumboTron from "./components/Jumbotron";
 import HumidityForm from "./components/HumidityForm";
 import Footer from "./components/Footer";
-import "./App.css";
 
 function App() {
   return (
